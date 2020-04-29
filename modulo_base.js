3dda4d0f66793dadd22ea38f448311f3f7fc56f1
jesus <jesus.e.marquez@gmail.com>
2020-04-28 21:30:13 -0500

    initial commit

---
+++ modulo_base.js
@@ -0,0 +1,9 @@
+//import{ name } from './modulo_dos.mjs'
+import{ calor } from './modulo_dos.mjs'
+import def from './modulo_dos.mjs'
+
+
+const saludo=prompt("que onda saludame prro");
+
+console.log(def);
+console.log ("esto es git")
---
+++ modulo_dos.mjs
@@ -0,0 +1,6 @@
+// export const name= 'jesus';
+expor const calor ='hace mucho calor';
+
+let valor = 250;
+
+export default valor;
